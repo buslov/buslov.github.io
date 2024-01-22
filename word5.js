e0f68edba0a01bccfe3d2f41aaea05333983fdd3
Artem Buslov <buslov.artem@gmail.com>
2024-01-22 21:10:56 +0300

    word5: исправлена проверка букв

--- word5.js
+++ word5.js
@@ -283,6 +283,9 @@ class Game {
                 }
             }
             for (let i = 0; i < 5; i++) {
+                if (result[i]) {
+                    continue;
+                }
                 for (let j = 0; j < 5; j++) {
                     if (used[j] != 0) {
                         continue;
